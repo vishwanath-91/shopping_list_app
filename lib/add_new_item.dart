8d677ce8dcc93a10df43e80d7f69b2d49838a41a
vishwanath nishad <om.vishwanath.91@gmail.com>
2024-01-26 21:57:44 +0530

    grocery_list_screen created

---
+++ lib/add_new_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddNewItem extends StatefulWidget {
+  const AddNewItem({super.key});
+
+  @override
+  State<AddNewItem> createState() => _AddNewItemState();
+}
+
+class _AddNewItemState extends State<AddNewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/grocery_list_screen.dart
+++ lib/grocery_list_screen.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/Model/grocery_item.dart';
+import 'package:shopping_list_app/add_new_item.dart';
 
-class GrocerylistScreen extends StatefulWidget {
-  const GrocerylistScreen({super.key});
+class GroceryListScreen extends StatefulWidget {
+  const GroceryListScreen({super.key});
 
   @override
-  State<GrocerylistScreen> createState() => _GrocerylistScreenState();
+  State<GroceryListScreen> createState() => _GroceryListScreenState();
 }
 
-class _GrocerylistScreenState extends State<GrocerylistScreen> {
+class _GroceryListScreenState extends State<GroceryListScreen> {
+  final List<GroceryItemModel> _groceryItemModel = [];
+
+  void _addItem() {
+    Navigator.of(context).push<GroceryItemModel>(MaterialPageRoute(
+      builder: (context) => const AddNewItem(),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Widget content = const Center(
+      child: Text("No items added yet"),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text("Your Grocery's"),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: content,
+    );
   }
 }
