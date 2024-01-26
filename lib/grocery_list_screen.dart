ed1c37d739c1ed7b254241f31fb1c2e2cefeb9a6
vishwanath nishad <om.vishwanath.91@gmail.com>
2024-01-26 21:11:50 +0530

    shopping list app model file and data file created

---
+++ lib/grocery_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GrocerylistScreen extends StatefulWidget {
+  const GrocerylistScreen({super.key});
+
+  @override
+  State<GrocerylistScreen> createState() => _GrocerylistScreenState();
+}
+
+class _GrocerylistScreenState extends State<GrocerylistScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
